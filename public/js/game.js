53021280c33e63d315bc06618ce249af4546a17c
AshishBardhan <ashbardhan@gmail.com>
2014-09-03 02:45:19 +0530

    Two Avatars + Move Gameplay

--- public/js/game.js
+++ public/js/game.js
@@ -12,9 +12,17 @@ var ladders = [
   [49,92],
   [62,80]
 ];
+var position = {
+  'p-1' : 0,
+  'p-2' : 0
+}
 
 var gamePlay = {
-  moveAvatar : function(fromY, fromX, toY, toX,speed){
+  moveAvatar : function(player, from, to, speed, callback){
+    var fromY = parseInt(from/10);
+    var toY = parseInt(to/10);
+    var fromX = from%10;
+    var toX = to%10;
     var distY = toY - fromY;
     var distX;
     if(distY % 2 == 0){
@@ -26,14 +34,71 @@ var gamePlay = {
       distX = -distX;
     distY = (distY <0?'-':'+')+ '=' + (55*(distY > 0 ? distY : -distY)) + 'px';
     distX = (distX <0?'-':'+')+ '=' + (57*(distX > 0 ? distX : -distX)) + 'px';
-    $('.game-player').animate({bottom: distY , left: distX},speed);
+    $('.game-player.p-'+player).animate({bottom: distY , left: distX},speed,(typeof callback != undefined)?callback:null);
   },
-  snakeBite : function(snakeBody){
-    for(var i=1;i<snakeBody.length;i++)
-      gamePlay.moveAvatar(parseInt(snakeBody[i-1]/10),snakeBody[i-1]%10,parseInt(snakeBody[i]/10),snakeBody[i]%10,200);
+  snakeBite : function(player,snakeBody){
+    $('.game-player.p-'+player+' .player-avatar').removeClass('normal').addClass('sad');
+    $('.game-player:not(.p-'+player+') .player-avatar').removeClass('normal').addClass('happy');
+    var callback = function(){
+      $('.game-player.p-'+player+' .player-avatar').addClass('normal').removeClass('sad');
+      $('.game-player:not(.p-'+player+') .player-avatar').addClass('normal').removeClass('happy');
+    }
+    var length = snakeBody.length-1;
+    for(var i=1;i<=length;i++)
+      gamePlay.moveAvatar(player,snakeBody[i-1],snakeBody[i],300,(i==length)?callback:null);
+    position['p-'+player] = snakeBody[length];
   },
-  ladderHit : function(ladder){
-    gamePlay.moveAvatar(parseInt(ladder[0]/10),ladder[0]%10,parseInt(ladder[1]/10),ladder[1]%10,200);
-  }
+  ladderHit : function(player,ladder){
+    $('.game-player.p-'+player+' .player-avatar').removeClass('normal').addClass('happy');
+    $('.game-player:not(.p-'+player+') .player-avatar').removeClass('normal').addClass('angry');
+    var callback = function(){
+      $('.game-player.p-'+player+' .player-avatar').addClass('normal').removeClass('happy');
+      $('.game-player:not(.p-'+player+') .player-avatar').addClass('normal').removeClass('angry');
+    }
+    gamePlay.moveAvatar(player,ladder[0],ladder[1],500,callback);
+    position['p-'+player] = ladder[1];
+  },
+  diceMove : function(player,dice){
+    var source = position['p-'+player];
+    var sourceX = parseInt(source%10);
+    var delay = 0;
+    if(source + dice <= 99){
+      var dest = (sourceX+dice <= 9)? (source+dice) : (source+(9-sourceX));
+      gamePlay.moveAvatar(player,source,dest,500);
+      delay += 500;
+      if(source + dice > dest){
+        gamePlay.moveAvatar(player,dest,dest+1,500);
+        delay += 500;
+        dest += 1;
+        if(source + dice > dest){
+          gamePlay.moveAvatar(player,dest,source+dice,500);
+          delay += 500;
+        }
+      }
+      position['p-'+player] = source + dice;
+      setTimeout(function(){
+        gamePlay.checkLadderHit(player);
+        gamePlay.checkSnakeBite(player);
+      },delay);
 
+    }  
+  },
+  checkLadderHit : function(player){
+    for(var i in ladders){
+      if(ladders[i][0] == position['p-'+player]){
+        gamePlay.ladderHit(player,ladders[i]);
+        return;
+      }
+    }
+    return;
+  },
+  checkSnakeBite : function(player){
+    for(var i in snakes){
+      if(snakes[i][0] == position['p-'+player]){
+        gamePlay.snakeBite(player,snakes[i]);
+        return;
+      }
+    }
+    return;
+  }
 };
